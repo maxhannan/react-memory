0cf7bd6e82c75b51bfd8dd9ca3712db25a13fb97
maxhannan <maxhannan@substanceseven.com>
2021-03-14 12:25:37 -0500

    inital game logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import {GiAllSeeingEye, GiAlgae, GiArmadillo, GiBatMask} from 'react-icons/gi'
+import { useState } from 'react'
+import uniqid from 'uniqid'
+import MemoryCard from './Components/MemoryCard'
 
+const cardArray = [
+  {
+    id: uniqid(),
+    icon: <GiAlgae/>,
+    text: 'Algae'
+  },
+  {
+    id: uniqid(),
+    icon: <GiAllSeeingEye/>,
+    text: 'All Seeing Eye'
+  },
+  {
+    id: uniqid(),
+    icon: <GiArmadillo/>,
+    text: 'Armadillo'
+  },
+  {
+    id: uniqid(),
+    icon: <GiBatMask/>,
+    text: 'Batman'
+  }
+]
 function App() {
+  const [cards, setCards] = useState(cardArray)
+  const [clickedItems, setClickedItems] = useState([])
+  const [gameOver, setGameOver] = useState(false)
+
+  const shuffle = (array) => {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  const handleClick = (e) => {
+    const targetId = e.target.closest('div').id
+    if(clickedItems.includes(targetId)){
+      console.log('gameOver')
+      setClickedItems([])
+      setGameOver(true)
+    }else{
+      setClickedItems([...clickedItems, targetId])
+    }
+   
+    console.log(clickedItems)
+    makeNewCards(cards)
+  }
+
+  const makeNewCards = (cards) => {
+    const NewCards = [...cards]
+    shuffle(NewCards)
+    setCards(NewCards)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style = {appStyle}>
+      <div className="gridWrapper" >
+      {gameOver && <h1>Game Over</h1>}
+       {!gameOver && cards.map(card => (
+         <MemoryCard card = {card} key = {card.id} handleClick = {handleClick}/>
+       ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const appStyle ={
+  display: 'flex',
+  justifyContent: 'center',
+  marginTop: '1em'
+}
---
+++ src/Components/MemoryCard.jsx
@@ -0,0 +1,36 @@
+
+const MemoryCard = ({card, handleClick}) => {
+  return ( 
+    <div style = {memoryCardStyle} id = {card.id} onClick = {(e) => handleClick(e)}>
+      <div id = {card.id}  className="iconHolder" style ={iconHolder}>
+        {card.icon}
+      </div>
+      <div id = {card.id}  className="textHolder" style ={textHolderStyle}>
+        {card.text}
+      </div>
+    </div>
+  );
+}
+ 
+export default MemoryCard;
+
+const memoryCardStyle = {
+  border: '1px solid #ccc',
+  height: '300px',
+  minWidth: '280px',
+  display: 'flex',
+  flexDirection: 'column'
+}
+const iconHolder = {
+  display: 'flex',
+  alignItems:'center',
+  justifyContent: 'center',
+  height: '75%',
+  fontSize: '100pt'
+}
+const textHolderStyle = {
+  display: 'flex',
+  alignItems:'center',
+  justifyContent: 'center',
+  fontSize: '20pt'
+}
